53c3e2417732ca77bcefea27ece4474c611e5670
Victor Marques <victyrmarques@gmail.com>
2023-01-30 22:29:28 -0300

    POM-7 - Ajuste CSS icon pokemon, local, árvore

--- src/components/Elements/IconLocation.vue
+++ src/components/Elements/IconLocation.vue
@@ -75,15 +75,15 @@
 		cursor: pointer;
 		visibility: hidden;
         margin-left: 0.6vw;
-        margin-top:-2vw
+        margin-top: -2vw
 	}
 	.icon-location .options img
 	{
 		width: 3vw;
 		filter: drop-shadow(0.2vw 0.2vw 0vw rgb(0, 0, 0));
-		margin: 0 1vw 0 -1vw !important;
+		margin: 0 1vw 0 -1vw;
 		background-color: rgb(255, 255, 255);
-		border-radius: 50% !important;
+		border-radius: 50%;
 	}
 	.icon-location:hover .options
 	{
