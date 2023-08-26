edde26e6d827c596a410eedfc34175095ac2e37b
Victor Marques <victyrmarques@gmail.com>
2023-08-26 10:05:12 -0300

    POM-15 - remoção de alteração do main.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,4 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-//createApp(App).mount('#app')
-const app = createApp(App);
-
-//app.config.errorHandler = () => null;
-app.config.warnHandler = () => null;
-
-app.mount("#app");
+createApp(App).mount('#app')
